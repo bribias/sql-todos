36279e2bc4ca474e116ba28b0f23292aec19ac53
Brianna Bias <brianna.bias@gmail.com>
2021-06-29 17:06:58 -0700

    fixed test issue so i dont have to comment out todo.js

--- data/create-tables.js
+++ data/create-tables.js
@@ -20,7 +20,7 @@ async function run() {
                 CREATE TABLE todos (
                     id SERIAL PRIMARY KEY NOT NULL,
                     todo VARCHAR(512) NOT NULL,
-                    completed VARCHAR(512)  NOT NULL,
+                    completed BOOLEAN  NOT NULL,
                     user_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
